326d6239ed8be56c2db2d3ced2b29594dabbbf23
Carlos <83241443+CarlosLopezLeal@users.noreply.github.com>
2024-10-03 14:24:04 +0200

    Add files via upload

---
+++ ej2.py
@@ -0,0 +1,10 @@
+'''Escribe una aplicación que pida la fecha actual (día y mes) y el hemisferio (Norte/Sur) e
+indique en qué estación se encuentra:
+a. Hemisferio Norte: Otoño (desde 23 Septiembre), Invierno (desde 21
+diciembre), Primavera (desde 21 Marzo), Verano (desde 21 Junio).
+b. Hemisferio Sur: Primavera (desde 23 Septiembre), Verano (desde 21
+diciembre), Otoño (desde 21 Marzo), Invierno (desde 21 Junio).'''
+
+dia=int(input("dime el dia que es: "))
+mes=int(input("dime el mes en el que estamos: "))
+
