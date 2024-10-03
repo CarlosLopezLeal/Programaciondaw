b6df19ea5837e777795ed956e9581ad5f34cbf2e
Carlos <83241443+CarlosLopezLeal@users.noreply.github.com>
2024-10-03 14:26:20 +0200

    Add files via upload

---
+++ programacion.py
@@ -0,0 +1,30 @@
+''' Escribe un programa que reciba las coordenadas de un punto respecto a los ejes XY y
+determine el cuadrante en el que se encuentra el punto. Si se encuentra sobre un eje
+debe indicar cuál..'''
+
+x = int(input("Introduce la coordenada X: "))
+y = int(input("Introduce la coordenada Y: "))
+
+if(x > 0) and (y > 0):
+    print("La coordenada está en el primer cuadrante")
+
+elif(x < 0) and (y > 0):
+    print("La coordenada está en el segundo cuadrante")
+
+elif (x < 0) and (y < 0):
+    print("El punto está en el tercer cuadrante.")
+
+elif (x > 0) and (y < 0):
+    print("El punto está en el cuarto cuadrante.")
+
+elif (x == 0) and (y != 0):
+       print("El punto está sobre el eje Y.")
+
+elif (y == 0) and (x != 0):
+    print("El punto está sobre el eje X.")
+
+else:
+    print("El punto está en el origen.")
+
+    
+
